599388e8060558a9b714072eb1cefd0a42f1ba9c
Jordan Lev <github@jordanlev.com>
2012-10-08 00:20:37 -0500

    refactored model save() method to use ADODB AutoExecute (instead of manually constructing our own SQL strings)

--- libraries/basic_crud_model.php
+++ libraries/basic_crud_model.php
@@ -30,7 +30,7 @@ class BasicCRUDModel {
 					$this->fields[] = $col->name;
 				}
 			}
-		}		
+		}
 	}
 	
 	public function findById($id) {
@@ -50,43 +50,26 @@ class BasicCRUDModel {
 	// (if its value is empty we INSERT, otherwise we UPDATE the record having that id).
 	//Returns the id of the inserted/updated record.
 	public function save($post) {
-		$vals = $this->fieldValsFromPost($post);
-		$id = isset($post[$this->pkid]) ? $post[$this->pkid] : null;
+		$record = $this->recordFromPost($post);
+		$id = isset($post[$this->pkid]) ? intval($post[$this->pkid]) : null;
 		
 		if (empty($id)) {
-			//add new record
-			$insert_fields = implode(', ', $this->fields);
-			$insert_placeholders = trim(str_repeat('?, ', count($this->fields)), ', ');
-			$sql = "INSERT INTO {$this->table}"
-				 . " ({$insert_fields})"
-			     . " VALUES ({$insert_placeholders})";
-			$this->db->Execute($sql, $vals);
+			$this->db->AutoExecute($this->table, $record, 'INSERT');
 			return $this->db->Insert_ID();
 		} else {
-			//update existing record
-			$placeholders = array();
-			foreach ($this->fields as $field) {
-				$placeholders[] = "{$field} = ?";
-			}
-			$update_field_string = implode(', ', $placeholders);
-			
-			$sql = "UPDATE {$this->table}"
-				 . " SET {$update_field_string}"
-				 . " WHERE {$this->pkid} = ?";
-			$vals[] = intval($id); //force integer type, otherwise ADODB escapes it as a string
-			$this->db->Execute($sql, $vals);
+			$this->db->AutoExecute($this->table, $record, 'UPDATE', "{$this->pkid}={$id}"); //NOTE that we already intval()'ed the $id.
 			return $id;
 		}
 	}
 	
-	private function fieldValsFromPost($post) {
-		$return_vals = array();
+	private function recordFromPost($post) {
+		$record = array();
 		foreach ($this->fields as $field) {
 			$val = array_key_exists($field, $post) ? $post[$field] : null;
 			$val = ($val === '') ? null : $val; //don't just check for empty() because then a '0' would erroneously become null!
-			$return_vals[] = $val;
+			$record[$field] = $val;
 		}
-		return $return_vals;
+		return $record;
 	}
 	
 }
